8aa9b113c40e46f83c5d11621f0cc5db229b840c
mlhomfeld <mlhomfeld@yahoo.com>
2017-08-15 10:23:06 -0500

    Created medicationController.php

---
+++ src/Controllers/medicationController.php
@@ -0,0 +1,195 @@
+<?php
+namespace FSS\Controllers;
+use FSS\Models\Medication;
+/**
+ * The controller for medication-related actions.
+ *
+ * Implements the ControllerInterface.
+ *
+ * Borrows from addressController
+ *
+ * @author Marshal
+ *        
+ */
+class MedicationController implements ControllerInterface
+{
+    // The DI container reference.
+    private $container;
+    /**
+     * The constructor that sets the DI Container reference and
+     * enable query logging if debug mode is true in settings.php
+     *
+     * @param unknown $c
+     */
+    public function __construct($c)
+    {
+        $this->container = $c;
+        if ($this->container['settings']['debug']) {
+            $this->container['logger']->debug(
+                "Enabling query log for the Medication Controller.");
+            $this->container['db']::enableQueryLog();
+        }
+    }
+    /**
+     *
+     * {@inheritdoc}
+     * @see \FSS\Controllers\ControllerInterface::read()
+     */
+    public function read($request, $response, $args)
+    {
+        $id = $args['id'];
+        $args['filter'] = "id";
+        $args['value'] = $id;
+        
+        // $this->container['logger']->info("Reading medication with id of $id");
+        $this->container['logger']->debug("Reading medication with id of $id");
+        
+        return $this->readAllWithFilter($request, $response, $args);
+    }
+    /**
+     *
+     * {@inheritdoc}
+     * @see \FSS\Controllers\ControllerInterface::readAll()
+     */
+    public function readAll($request, $response, $args)
+    {
+        $records = Medication::all();
+        $this->container['logger']->debug("All languages query: ",
+            $this->container['db']::getQueryLog());
+        // $records = Medication::all();
+        return $response->withJson(
+            [
+                "success" => true,
+                "message" => "All Medications returned",
+                "data" => $records
+            ], 200, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
+    }
+    /**
+     *
+     * {@inheritdoc}
+     * @see \FSS\Controllers\ControllerInterface::readAllWithFilter()
+     */
+    public function readAllWithFilter($request, $response, $args)
+    {
+        $filter = $args['filter'];
+        $value = $args['value'];
+        
+        try {
+            Medication::validateColumn('medication', $filter, $this->container);
+            $records = Medication::where($filter, $value)->get();
+            $this->container['logger']->debug("Medication filter query: ",
+                $this->container['db']::getQueryLog());
+            if ($records->isEmpty()) {
+                return $response->withJson(
+                    [
+                        "success" => true,
+                        "message" => "No Medication found",
+                        "data" => $records
+                    ], 404);
+            }
+            return $response->withJson(
+                [
+                    "success" => true,
+                    "message" => "Filtered Medications by $filter",
+                    "data" => $records
+                ], 200, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
+        } catch (\Exception $e) {
+            return $response->withJson(
+                [
+                    "success" => false,
+                    "message" => "Error occured: " . $e->getMessage()
+                ], 400);
+        }
+    }
+    /**
+     *
+     * {@inheritdoc}
+     * @see \FSS\Controllers\ControllerInterface::create()
+     */
+    public function create($request, $response, $args)
+    {
+        // Make sure the frontend only puts the name attribute
+        // on form elements that actually contain data
+        // for the record.
+        $recordData = $request->getParsedBody();
+        try {
+            foreach ($recordData as $key => $val) {
+                Medication::validateColumn('medication', $key, $this->container);
+            }
+            $recordId = Medication::insertGetId($recordData);
+            $this->container['logger']->debug("Medication create query: ",
+                $this->container['db']::getQueryLog());
+            return $response->withJson(
+                [
+                    "success" => true,
+                    "message" => "Medication $recordId has been created."
+                ], 200, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
+        } catch (\Exception $e) {
+            return $response->withJson(
+                [
+                    "success" => false,
+                    "message" => "Error occured: " . $e->getMessage()
+                ], 400);
+        }
+    }
+    /**
+     *
+     * {@inheritdoc}
+     * @see \FSS\Controllers\ControllerInterface::update()
+     */
+    public function update($request, $response, $args)
+    {
+        // $id = $args['id'];
+        $recordData = $request->getParsedBody();
+        try {
+            $updateData = [];
+            foreach ($recordData as $key => $val) {
+                Medication::validateColumn('medication', $key, $this->container);
+                $updateData = array_merge($updateData,
+                    [
+                        $key => $val
+                    ]);
+            }
+            $recordId = Medication::update($updateData);
+            $this->container['logger']->debug("Medication update query: ",
+                $this->container['db']::getQueryLog());
+            return $response->withJson(
+                [
+                    "success" => true,
+                    "message" => "Updated Medication $recordId"
+                ], 200, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
+        } catch (\Exception $e) {
+            return $response->withJson(
+                [
+                    "success" => false,
+                    "message" => "Error occured: " . $e->getMessage()
+                ], 400);
+        }
+    }
+    /**
+     *
+     * {@inheritdoc}
+     * @see \FSS\Controllers\ControllerInterface::delete()
+     */
+    public function delete($request, $response, $args)
+    {
+        $id = $args['id'];
+        try {
+            $record = Medication::findOrFail($id);
+            $record->delete();
+            $this->container['logger']->debug("Medication delete query: ",
+                $this->container['db']::getQueryLog());
+            return $response->withJson(
+                [
+                    "success" => true,
+                    "message" => "Deleted Medication $id"
+                ], 200, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
+        } catch (\Exception $e) {
+            return $response->withJson(
+                [
+                    "success" => false,
+                    "message" => "Medication not found"
+                ], 404);
+        }
+    }
+}
