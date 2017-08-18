b18a02009eca15b09752b5feeee052d562e81f86
mlhomfeld <mlhomfeld@yahoo.com>
2017-08-18 17:24:55 -0500

    Create county_dataController.php

---
+++ src/Controllers/county_dataController.php
@@ -0,0 +1,195 @@
+<?php
+namespace FSS\Controllers;
+use FSS\Models\County_data;
+/**
+ * The controller for county_data-related actions.
+ *
+ * Implements the ControllerInterface.
+ *
+ * Borrows from addressController
+ *
+ * @author Marshal
+ *        
+ */
+class County_dataController implements ControllerInterface
+{
+    // The DI container reference.
+    private $container;
+    /**
+     * The constructor that sets the DI Container reference and
+     * enable query logging if debug mode is true in settings.php
+     *
+     * @param unknown $c
+     */
+    public function __construct($c)
+    {
+        $this->container = $c;
+        if ($this->container['settings']['debug']) {
+            $this->container['logger']->debug(
+                "Enabling query log for the county_data Controller.");
+            $this->container['db']::enableQueryLog();
+        }
+    }
+    /**
+     *
+     * {@inheritdoc}
+     * @see \FSS\Controllers\ControllerInterface::read()
+     */
+    public function read($request, $response, $args)
+    {
+        $id = $args['id'];
+        $args['filter'] = "id";
+        $args['value'] = $id;
+        
+        // $this->container['logger']->info("Reading county_data with id of $id");
+        $this->container['logger']->debug("Reading county_data with id of $id");
+        
+        return $this->readAllWithFilter($request, $response, $args);
+    }
+    /**
+     *
+     * {@inheritdoc}
+     * @see \FSS\Controllers\ControllerInterface::readAll()
+     */
+    public function readAll($request, $response, $args)
+    {
+        $records = County_data::all();
+        $this->container['logger']->debug("All county_data query: ",
+            $this->container['db']::getQueryLog());
+        // $records = County_data::all();
+        return $response->withJson(
+            [
+                "success" => true,
+                "message" => "All county_data returned",
+                "data" => $records
+            ], 200, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
+    }
+    /**
+     *
+     * {@inheritdoc}
+     * @see \FSS\Controllers\ControllerInterface::readAllWithFilter()
+     */
+    public function readAllWithFilter($request, $response, $args)
+    {
+        $filter = $args['filter'];
+        $value = $args['value'];
+        
+        try {
+            County_data::validateColumn('county_data', $filter, $this->container);
+            $records = County_data::where($filter, $value)->get();
+            $this->container['logger']->debug("County_data filter query: ",
+                $this->container['db']::getQueryLog());
+            if ($records->isEmpty()) {
+                return $response->withJson(
+                    [
+                        "success" => true,
+                        "message" => "No County_data found",
+                        "data" => $records
+                    ], 404);
+            }
+            return $response->withJson(
+                [
+                    "success" => true,
+                    "message" => "Filtered County_data by $filter",
+                    "data" => $records
+                ], 200, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
+        } catch (\Exception $e) {
+            return $response->withJson(
+                [
+                    "success" => false,
+                    "message" => "Error occured: " . $e->getMessage()
+                ], 400);
+        }
+    }
+    /**
+     *
+     * {@inheritdoc}
+     * @see \FSS\Controllers\ControllerInterface::create()
+     */
+    public function create($request, $response, $args)
+    {
+        // Make sure the frontend only puts the name attribute
+        // on form elements that actually contain data
+        // for the record.
+        $recordData = $request->getParsedBody();
+        try {
+            foreach ($recordData as $key => $val) {
+                County_data::validateColumn('county_data', $key, $this->container);
+            }
+            $recordId = County_data::insertGetId($recordData);
+            $this->container['logger']->debug("County_data create query: ",
+                $this->container['db']::getQueryLog());
+            return $response->withJson(
+                [
+                    "success" => true,
+                    "message" => "County_data $recordId has been created."
+                ], 200, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
+        } catch (\Exception $e) {
+            return $response->withJson(
+                [
+                    "success" => false,
+                    "message" => "Error occured: " . $e->getMessage()
+                ], 400);
+        }
+    }
+    /**
+     *
+     * {@inheritdoc}
+     * @see \FSS\Controllers\ControllerInterface::update()
+     */
+    public function update($request, $response, $args)
+    {
+        // $id = $args['id'];
+        $recordData = $request->getParsedBody();
+        try {
+            $updateData = [];
+            foreach ($recordData as $key => $val) {
+                County_data::validateColumn('county_data', $key, $this->container);
+                $updateData = array_merge($updateData,
+                    [
+                        $key => $val
+                    ]);
+            }
+            $recordId = County_data::update($updateData);
+            $this->container['logger']->debug("County_data update query: ",
+                $this->container['db']::getQueryLog());
+            return $response->withJson(
+                [
+                    "success" => true,
+                    "message" => "Updated County_data $recordId"
+                ], 200, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
+        } catch (\Exception $e) {
+            return $response->withJson(
+                [
+                    "success" => false,
+                    "message" => "Error occured: " . $e->getMessage()
+                ], 400);
+        }
+    }
+    /**
+     *
+     * {@inheritdoc}
+     * @see \FSS\Controllers\ControllerInterface::delete()
+     */
+    public function delete($request, $response, $args)
+    {
+        $id = $args['id'];
+        try {
+            $record = County_data::findOrFail($id);
+            $record->delete();
+            $this->container['logger']->debug("County_data delete query: ",
+                $this->container['db']::getQueryLog());
+            return $response->withJson(
+                [
+                    "success" => true,
+                    "message" => "Deleted County_data $id"
+                ], 200, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
+        } catch (\Exception $e) {
+            return $response->withJson(
+                [
+                    "success" => false,
+                    "message" => "County_data not found"
+                ], 404);
+        }
+    }
+}
