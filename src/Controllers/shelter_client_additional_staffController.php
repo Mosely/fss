8aa755101a9690cdb293f803312631e082e23445
mlhomfeld <mlhomfeld@yahoo.com>
2017-08-18 20:08:24 -0500

    Create shelter_client_additional_staffController.php

---
+++ src/Controllers/shelter_client_additional_staffController.php
@@ -0,0 +1,198 @@
+<?php
+namespace FSS\Controllers;
+use FSS\Models\Shelter_client_additional_staff;
+/**
+ * The controller for shelter_client_additional_staff-related actions.
+ *
+ * Implements the ControllerInterface.
+ *
+ * Borrows from addressController
+ *
+ * @author Marshal
+ *        
+ */
+class Shelter_client_additional_staffController implements ControllerInterface
+{
+    // The DI container reference.
+    private $container;
+    /**
+     * The constructor that sets the DI Container reference and
+     * enable query logging if debug mode is true in settings.php
+     *
+     * @param unknown $c
+     */
+    public function __construct($c)
+    {
+        $this->container = $c;
+        if ($this->container['settings']['debug']) {
+            $this->container['logger']->debug(
+                "Enabling query log for the Shelter_client_additional_staff Controller.");
+            $this->container['db']::enableQueryLog();
+        }
+    }
+    /**
+     *
+     * {@inheritdoc}
+     * @see \FSS\Controllers\ControllerInterface::read()
+     */
+    public function read($request, $response, $args)
+    {
+        $id = $args['id'];
+        $args['filter'] = "id";
+        $args['value'] = $id;
+        
+        // $this->container['logger']->info("Reading shelter_client_additional_staff with id of $id");
+        $this->container['logger']->debug("Reading shelter_client_additional_staff with id of $id");
+        
+        return $this->readAllWithFilter($request, $response, $args);
+    }
+    /**
+     *
+     * {@inheritdoc}
+     * @see \FSS\Controllers\ControllerInterface::readAll()
+     */
+    public function readAll($request, $response, $args)
+    {
+        $records = Shelter_client_additional_staff::all();
+        $this->container['logger']->debug("All shelter_client_additional_staff query: ",
+            $this->container['db']::getQueryLog());
+        // $records = Shelter_client_additional_staff::all();
+        return $response->withJson(
+            [
+                "success" => true,
+                "message" => "All shelter_client_additional_staff returned",
+                "data" => $records
+            ], 200, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
+    }
+    /**
+     *
+     * {@inheritdoc}
+     * @see \FSS\Controllers\ControllerInterface::readAllWithFilter()
+     */
+    public function readAllWithFilter($request, $response, $args)
+    {
+        $filter = $args['filter'];
+        $value = $args['value'];
+        
+        try {
+            Shelter_client_additional_staff::validateColumn('shelter_client_additional_staff',
+                $filter, $this->container);
+            $records = Shelter_client_additional_staff::where($filter, $value)->get();
+            $this->container['logger']->debug("Shelter_client_additional_staff filter query: ",
+                $this->container['db']::getQueryLog());
+            if ($records->isEmpty()) {
+                return $response->withJson(
+                    [
+                        "success" => true,
+                        "message" => "No Shelter_client_additional_staff found",
+                        "data" => $records
+                    ], 404);
+            }
+            return $response->withJson(
+                [
+                    "success" => true,
+                    "message" => "Filtered Shelter_client_additional_staff by $filter",
+                    "data" => $records
+                ], 200, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
+        } catch (\Exception $e) {
+            return $response->withJson(
+                [
+                    "success" => false,
+                    "message" => "Error occured: " . $e->getMessage()
+                ], 400);
+        }
+    }
+    /**
+     *
+     * {@inheritdoc}
+     * @see \FSS\Controllers\ControllerInterface::create()
+     */
+    public function create($request, $response, $args)
+    {
+        // Make sure the frontend only puts the name attribute
+        // on form elements that actually contain data
+        // for the record.
+        $recordData = $request->getParsedBody();
+        try {
+            foreach ($recordData as $key => $val) {
+                Shelter_client_additional_staff::validateColumn('shelter_client_additional_staff',
+                    $key, $this->container);
+            }
+            $recordId = Shelter_client_additional_staff::insertGetId($recordData);
+            $this->container['logger']->debug("Shelter_client_additional_staff create query: ",
+                $this->container['db']::getQueryLog());
+            return $response->withJson(
+                [
+                    "success" => true,
+                    "message" => "Shelter_client_additional_staff $recordId has been created."
+                ], 200, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
+        } catch (\Exception $e) {
+            return $response->withJson(
+                [
+                    "success" => false,
+                    "message" => "Error occured: " . $e->getMessage()
+                ], 400);
+        }
+    }
+    /**
+     *
+     * {@inheritdoc}
+     * @see \FSS\Controllers\ControllerInterface::update()
+     */
+    public function update($request, $response, $args)
+    {
+        // $id = $args['id'];
+        $recordData = $request->getParsedBody();
+        try {
+            $updateData = [];
+            foreach ($recordData as $key => $val) {
+                Shelter_client_additional_staff::validateColumn('shelter_client_additional_staff',
+                    $key, $this->container);
+                $updateData = array_merge($updateData,
+                    [
+                        $key => $val
+                    ]);
+            }
+            $recordId = Shelter_client_additional_staff::update($updateData);
+            $this->container['logger']->debug("Shelter_client_additional_staff update query: ",
+                $this->container['db']::getQueryLog());
+            return $response->withJson(
+                [
+                    "success" => true,
+                    "message" => "Updated Shelter_client_additional_staff $recordId"
+                ], 200, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
+        } catch (\Exception $e) {
+            return $response->withJson(
+                [
+                    "success" => false,
+                    "message" => "Error occured: " . $e->getMessage()
+                ], 400);
+        }
+    }
+    /**
+     *
+     * {@inheritdoc}
+     * @see \FSS\Controllers\ControllerInterface::delete()
+     */
+    public function delete($request, $response, $args)
+    {
+        $id = $args['id'];
+        try {
+            $record = Shelter_client_additional_staff::findOrFail($id);
+            $record->delete();
+            $this->container['logger']->debug("Shelter_client_additional_staff delete query: ",
+                $this->container['db']::getQueryLog());
+            return $response->withJson(
+                [
+                    "success" => true,
+                    "message" => "Deleted Shelter_client_additional_staff $id"
+                ], 200, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
+        } catch (\Exception $e) {
+            return $response->withJson(
+                [
+                    "success" => false,
+                    "message" => "Shelter_client_additional_staff not found"
+                ], 404);
+        }
+    }
+}
