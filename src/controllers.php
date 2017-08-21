385fb303df315815da6eb13800634b18daa9947b
mlhomfeld <mlhomfeld@yahoo.com>
2017-08-20 22:34:56 -0500

    Added remaining controller definitions

--- src/controllers.php
+++ src/controllers.php
@@ -42,3 +42,85 @@ $container['Client_languageController'] = function ($c) {
 $container['CounseleeController'] = function ($c) {
     return new FSS\Controllers\counseleeController($c);
 };
+$container['Counselee_childController'] = function ($c) {
+    return new FSS\Controllers\counseleeController($c);
+};
+$container['Counselee_child_bio_parentController'] = function ($c) {
+    return new FSS\Controllers\counseleeController($c);
+};
+$container['Counselee_child_guardianController'] = function ($c) {
+    return new FSS\Controllers\counseleeController($c);
+};
+$container['Counselee_child_siblingController'] = function ($c) {
+    return new FSS\Controllers\counseleeController($c);
+};
+$container['Counselee_counseling_topicController'] = function ($c) {
+    return new FSS\Controllers\counseleeController($c);
+};
+$container['Counselee_drug_useController'] = function ($c) {
+    return new FSS\Controllers\counseleeController($c);
+};
+$container['Counselee_medicationController'] = function ($c) {
+    return new FSS\Controllers\counseleeController($c);
+};
+$container['Counseling_topicController'] = function ($c) {
+    return new FSS\Controllers\counseleeController($c);
+};
+$container['County_dataController'] = function ($c) {
+    return new FSS\Controllers\counseleeController($c);
+};
+$container['Drug_useController'] = function ($c) {
+    return new FSS\Controllers\counseleeController($c);
+};
+$container['Funding_sourceController'] = function ($c) {
+    return new FSS\Controllers\counseleeController($c);
+};
+$container['Identity_preferenceController'] = function ($c) {
+    return new FSS\Controllers\counseleeController($c);
+};
+$container['Military_discharge_typeController'] = function ($c) {
+    return new FSS\Controllers\counseleeController($c);
+};
+$container['PersonController'] = function ($c) {
+    return new FSS\Controllers\counseleeController($c);
+};
+$container['Person_addressController'] = function ($c) {
+    return new FSS\Controllers\counseleeController($c);
+};
+$container['Person_phoneController'] = function ($c) {
+    return new FSS\Controllers\counseleeController($c);
+};
+$container['PhoneController'] = function ($c) {
+    return new FSS\Controllers\counseleeController($c);
+};
+$container['RoleController'] = function ($c) {
+    return new FSS\Controllers\counseleeController($c);
+};
+$container['SchoolController'] = function ($c) {
+    return new FSS\Controllers\counseleeController($c);
+};
+$container['Shelter_clientController'] = function ($c) {
+    return new FSS\Controllers\counseleeController($c);
+};
+$container['Shelter_client_additional_staffController'] = function ($c) {
+    return new FSS\Controllers\counseleeController($c);
+};
+$container['Shelter_client_funding_sourceController'] = function ($c) {
+    return new FSS\Controllers\counseleeController($c);
+};
+$container['Shelter_client_identity_preferenceController'] = function ($c) {
+    return new FSS\Controllers\counseleeController($c);
+};
+$container['State_dataController'] = function ($c) {
+    return new FSS\Controllers\counseleeController($c);
+};
+$container['User_roleController'] = function ($c) {
+    return new FSS\Controllers\counseleeController($c);
+};
+$container['User_viewController'] = function ($c) {
+    return new FSS\Controllers\counseleeController($c);
+};
+$container['VeteranController'] = function ($c) {
+    return new FSS\Controllers\counseleeController($c);
+};
+
