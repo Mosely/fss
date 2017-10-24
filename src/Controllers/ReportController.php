9d811dd29c32147def3853ecf2f32808f022acbc
djhiggs <djhiggs1@gmail.com>
2017-10-23 22:50:30 -0500

    Testing Rest of Report Generation Functionality

--- src/Controllers/ReportController.php
+++ src/Controllers/ReportController.php
@@ -40,12 +40,12 @@ class ReportController implements ControllerInterface {
     {
         $reportJson = $this->read($request, $response, $args)->getBody();
         $report = json_decode($reportJson, false);
-        print "<pre>";
-        print_r($reportJson);
-        print_r($report);
-        print "</pre>";
+        //print "<pre>";
+        //print_r($reportJson);
+        //print_r($report);
+        //print "</pre>";
         $columns = $report->data[0]->report_column;
-        /*$query = Report::run($columns);
+        $query = Report::run($columns);
         
         try {
             $records = $query->get();
@@ -64,7 +64,7 @@ class ReportController implements ControllerInterface {
                     "success" => false,
                     "message" => "Error occured: " . $e->getMessage()
                 ], 400);
-        }*/
+        }
     }
     
     public function read($request, $response, $args)
