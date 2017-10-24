5f0bfd1295c48de32565e023549d3a92258dcd79
djhiggs <djhiggs1@gmail.com>
2017-10-23 23:23:50 -0500

    More Debugging of the Notices

--- src/Controllers/ReportController.php
+++ src/Controllers/ReportController.php
@@ -40,10 +40,10 @@ class ReportController implements ControllerInterface {
     {
         $reportJson = $this->read($request, $response, $args)->getBody();
         $report = json_decode($reportJson, false);
-        //print "<pre>";
-        //print_r($reportJson);
-        //print_r($report);
-        //print "</pre>";
+        print "<pre>";
+        print_r($reportJson);
+        print_r($report);
+        print "</pre>";
         $columns = $report->data[0]->report_column;
         print "<pre>";
         print_r($columns);
