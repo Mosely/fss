d789f27e436ed9f3eca5f9ff98259a8304e561f7
mlhomfeld <mlhomfeld@yahoo.com>
2017-11-11 16:59:33 -0600

    Added limit(200) to readAllWithFilter

--- src/Controllers/UserViewController.php
+++ src/Controllers/UserViewController.php
@@ -104,7 +104,7 @@ class UserViewController implements ControllerInterface
         try {
             UserView::validateColumn('UserView', $filter, $this->logger,
                 $this->cache, $this->db);
-            $records = UserView::where($filter, $value)->get();
+            $records = UserView::where($filter, $value)->limit(200)->get();
             $this->logger->debug("UserView filter query: ",
                 $this->db::getQueryLog());
             if ($records->isEmpty()) {
