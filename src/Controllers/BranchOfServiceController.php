e54503ede608ea45b476f9d56c660b981714ba30
mlhomfeld <mlhomfeld@yahoo.com>
2017-11-11 15:28:34 -0600

    Added limit(200) to readAllWithFilter

--- src/Controllers/BranchOfServiceController.php
+++ src/Controllers/BranchOfServiceController.php
@@ -97,7 +97,7 @@ class BranchOfServiceController implements ControllerInterface
         try {
             BranchOfService::validateColumn('branch_of_service', $filter,
                 $this->container);
-            $records = BranchOfService::where($filter, $value)->get();
+            $records = BranchOfService::where($filter, $value)->limit(200)->get();
             if ($records->isEmpty()) {
                 return $response->withJson(
                     [
