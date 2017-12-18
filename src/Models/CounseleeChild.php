5b2a917f838ab64a4ea1adc435a10c750003a00f
mlhomfeld <mlhomfeld@yahoo.com>
2017-12-17 19:59:28 -0600

    Added relationship handling

--- src/Models/CounseleeChild.php
+++ src/Models/CounseleeChild.php
@@ -20,4 +20,29 @@ class CounseleeChild extends AbstractModel
         'long_standing_illnesses',
         'who_else_raised_child'
     );
+    
+    public function Counselee()
+    {
+        return $this->belongsTo('FSS\Models\Counselee');
+    }
+    
+    public function CounseleeChildBioParent()
+    {
+        return $this->hasMany('FSS\Models\CounseleeChildBioParent');
+    }
+    
+    public function CounseleeChildGuardian()
+    {
+        return $this->hasMany('FSS\Models\CounseleeChildGuardian');
+    }
+    
+    public function CounseleeChildSibling()
+    {
+        return $this->hasMany('FSS\Models\CounseleeChildSibling');
+    }
+    
+    public function School()
+    {
+        return $this->belongsTo('FSS\Models\School');
+    }
 }
