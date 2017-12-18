94cf57e27670a5129734d2b60779b6fd3927874d
mlhomfeld <mlhomfeld@yahoo.com>
2017-12-17 18:02:40 -0600

    Added relationship handling to BranchOfService.php

--- src/Models/BranchOfService.php
+++ src/Models/BranchOfService.php
@@ -13,5 +13,9 @@ class BranchOfService extends AbstractModel
     // The table for this model
     protected $table = 'branch_of_service';
     
+    public function Veteran()
+    {
+        return $this->hasMany('FSS\Models\Veteran');
+    }
     // getters and setters if you want and other logic
 }
