a70da1b9c3689c17258adb99237b136a2923a86a
djhiggs <djhiggs1@gmail.com>
2017-12-19 19:47:29 -0600

    Testing Relationship Handling in Veteran Controller

--- src/Controllers/VeteranController.php
+++ src/Controllers/VeteranController.php
@@ -104,7 +104,12 @@ class VeteranController implements ControllerInterface
         try {
             Veteran::validateColumn('veteran', $filter, $this->logger,
                 $this->cache, $this->db);
-            $records = Veteran::where($filter, $value)->limit(200)->get();
+            $records = Veteran::with(
+                    [
+                        'branch_of_service', 
+                        'military_discharge_type'
+                    ]
+                )->where($filter, $value)->limit(200)->get();
             $this->logger->debug("Veteran filter query: ",
                 $this->db::getQueryLog());
             if ($records->isEmpty()) {
