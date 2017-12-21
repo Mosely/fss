1645ca162bb44884bb70dc268afdff1ba47fe686
bjbrookz <bj@bulldance.net>
2017-12-21 10:26:48 -0600

    Swagged

--- src/Models/Medication.php
+++ src/Models/Medication.php
@@ -1,11 +1,20 @@
 <?php
 namespace FSS\Models;
 
+use Swagger\Annotations as SWG;
 /**
  * The "medication" model.
  *
  * @author Dewayne
- *        
+ *  
+ * @SWG\Model(
+ *     id="medication",
+ *     @SWG\Property(name="id", type="integer", required=true),
+ *     @SWG\Property(name="name", type="string", required=true),
+ *     @SWG\Property(name="created_at", type="integer", required=true), 
+ *     @SWG\Property(name="updated_at", type="integer", required=true), 
+ *     @SWG\Property(name="updated_by", type="integer", required=true)
+ * )
  */
 class Medication extends AbstractModel
 {
