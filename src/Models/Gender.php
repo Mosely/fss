78db85fe395f7d229a657dd38b8390a2bf2b6703
bjbrookz <bj@bulldance.net>
2017-12-21 10:20:36 -0600

    Swagged

--- src/Models/Gender.php
+++ src/Models/Gender.php
@@ -1,11 +1,20 @@
 <?php
 namespace FSS\Models;
 
+use Swagger\Annotations as SWG;
 /**
  * The "gender" model.
  *
  * @author Dewayne
  *        
+ * @SWG\Model(
+ *     id="gender",
+ *     @SWG\Property(name="id", type="integer", required=true),
+ *     @SWG\Property(name="name", type="string", required=true),
+ *     @SWG\Property(name="created_at", type="integer", required=true), 
+ *     @SWG\Property(name="updated_at", type="integer", required=true), 
+ *     @SWG\Property(name="updated_by", type="integer", required=true)
+ * )
  */
 class Gender extends AbstractModel
 {
