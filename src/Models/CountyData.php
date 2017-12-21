10ba35876df89e0a5b172dc26dc15ef911b04e80
bjbrookz <bj@bulldance.net>
2017-12-21 10:12:15 -0600

    Swagged

--- src/Models/CountyData.php
+++ src/Models/CountyData.php
@@ -1,11 +1,20 @@
 <?php
 namespace FSS\Models;
 
+use Swagger\Annotations as SWG;
 /**
  * The "county_data" model.
  *
  * @author Dewayne
- *        
+ *       
+ * @SWG\Model(
+ *     id="county_data",
+ *     @SWG\Property(name="id", type="integer", required=true),
+ *     @SWG\Property(name="name", type="string", required=true),
+ *     @SWG\Property(name="created_at", type="integer", required=true), 
+ *     @SWG\Property(name="updated_at", type="integer", required=true), 
+ *     @SWG\Property(name="updated_by", type="integer", required=true)
+ * )
  */
 class CountyData extends AbstractModel
 {
