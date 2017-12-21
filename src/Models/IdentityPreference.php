c7e7ef3d886890b65d5fc0252f51723fd55d8d02
bjbrookz <bj@bulldance.net>
2017-12-21 10:24:32 -0600

    Swagged

--- src/Models/IdentityPreference.php
+++ src/Models/IdentityPreference.php
@@ -1,11 +1,21 @@
 <?php
 namespace FSS\Models;
 
+use Swagger\Annotation as SWG;
 /**
  * The "identity_preference" model.
  *
  * @author Dewayne
  *        
+ * @SWG\Model(
+ *     id="identity_preference",
+ *     @SWG\Property(name="id", type="integer", required=true),
+ *     @SWG\Property(name="name", type="string", required=true),
+ *     @SWG\Property(name="description", type="string", required=false),
+ *     @SWG\Property(name="created_at", type="integer", required=true), 
+ *     @SWG\Property(name="updated_at", type="integer", required=true), 
+ *     @SWG\Property(name="updated_by", type="integer", required=true)
+ * )
  */
 class IdentityPreference extends AbstractModel
 {
