f841f8a332b95a785238d2bbf93dfb18b6515e73
bjbrookz <bj@bulldance.net>
2017-12-21 09:51:50 -0600

    Swagged

--- src/Models/CounseleeChildGuardian.php
+++ src/Models/CounseleeChildGuardian.php
@@ -1,11 +1,25 @@
 <?php
 namespace FSS\Models;
 
+use Swagger\Annotations as SWG;
 /**
  * The "counselee_child_guardian" model.
  *
  * @author Dewayne
- *        
+ *   
+ * @SWG\Model(
+ *     id="counselee_child_guardian",
+ *     @SWG\Property(name="id", type="integer", required=true),
+ *     @SWG\Property(name="counselee_child_id", type="integer", required=true),
+ *     @SWG\Property(name="name", type="string", required=true),
+ *     @SWG\Property(name="age", type="integer", required=true),
+ *     @SWG\Property(name="occupation", type="string", required=true),
+ *     @SWG\Property(name="is_currently_living_with_child", type="boolean", required=true),
+ *     @SWG\Property(name="date_first_lived_with_child", type="date", required=true),
+ *     @SWG\Property(name="created_at", type="integer", required=true), 
+ *     @SWG\Property(name="updated_at", type="integer", required=true), 
+ *     @SWG\Property(name="updated_by", type="integer", required=false)
+ * )  
  */
 class CounseleeChildGuardian extends AbstractModel
 {
