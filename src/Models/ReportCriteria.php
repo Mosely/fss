8377f4c668a87bd30b63fc79b97c9b2a6f546d1c
bjbrookz <bj@bulldance.net>
2017-12-21 10:46:48 -0600

    Swagged

--- src/Models/ReportCriteria.php
+++ src/Models/ReportCriteria.php
@@ -1,6 +1,23 @@
 <?php
 namespace FSS\Models;
 
+use Swagger\Annotations as SWG;
+/**
+ * The "report criteria" model.
+ *
+ * @author Dewayne
+ *        
+ * @SWG\Model(
+ *     id="report_critera",
+ *     @SWG\Property(name="id", type="integer", required=true),
+ *     @SWG\Property(name="report_column_id", type="integer", required=true),
+ *     @SWG\Property(name="relation", type="enum", required=true),
+ *     @SWG\Property(name="criteria_value", type="string", required=true),
+ *     @SWG\Property(name="created_at", type="integer", required=true), 
+ *     @SWG\Property(name="updated_at", type="integer", required=true), 
+ *     @SWG\Property(name="updated_by", type="integer", required=true)
+ * )
+ */
 class ReportCriteria extends AbstractModel
 {
 
