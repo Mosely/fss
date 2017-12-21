f8c3d726a4a18aaf38c92ec1bf5b76c9d98dcd8e
bjbrookz <bj@bulldance.net>
2017-12-21 10:11:02 -0600

    Swagged

--- src/Models/CounselingTopic.php
+++ src/Models/CounselingTopic.php
@@ -1,11 +1,21 @@
 <?php
 namespace FSS\Models;
 
+use Swagger\Annotations as SWG;
 /**
  * The "counseling_topic" model.
  *
  * @author Dewayne
- *        
+ *     
+ * @SWG\Model(
+ *     id="counseling_topic",
+ *     @SWG\Property(name="id", type="integer", required=true),
+ *     @SWG\Property(name="topic", type="string", required=true),
+ *     @SWG\Property(name="description", type="string", required=false),
+ *     @SWG\Property(name="created_at", type="integer", required=true), 
+ *     @SWG\Property(name="updated_at", type="integer", required=true), 
+ *     @SWG\Property(name="updated_by", type="integer", required=true)
+ * )
  */
 class CounselingTopic extends AbstractModel
 {
