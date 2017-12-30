d7d1fb54f385854a7e289bfc90abdbc7eae3ef20
djhiggs <djhiggs1@gmail.com>
2017-12-29 22:37:28 -0600

    Another validateColumn Fix

--- src/Models/AbstractModel.php
+++ src/Models/AbstractModel.php
@@ -40,7 +40,7 @@ abstract class AbstractModel extends Model
         Logger $logger, Cache $cache, Manager $db)
     {
         $columns = null;
-        $theTable = getTableName();
+        $theTable = AbstractModel::getTableName();
         if (($cacheValue = $cache->get($theTable)) != false) {
             $logger->debug("Retrieved $theTable column listing from cache.");
             $columns = $cacheValue;
