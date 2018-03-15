30530eb3e50e46e39bfa2ba6e37959b5412f54da
mlhomfeld <mlhomfeld@yahoo.com>
2018-03-15 14:49:16 -0500

    Updated read/readAllWithFilter

--- src/Controllers/FundingSourceController.php
+++ src/Controllers/FundingSourceController.php
@@ -91,9 +91,9 @@ class FundingSourceController extends AbstractController
         ResponseInterface $response, array $args): ResponseInterface
     {
         $id = $args['id'];
-        $args['filter'] = "id";
-        $args['value'] = $id;
-        
+        $params = ['id', $id];
+        $request = $request->withAttribute('params', 
+            implode('/', $params));
         // $this->logger->info("Reading FundingSource with id of $id");
         $this->logger->debug("Reading FundingSource with id of $id");
         
@@ -161,14 +161,30 @@ class FundingSourceController extends AbstractController
     public function readAllWithFilter(ServerRequestInterface $request,
         ResponseInterface $response, array $args): ResponseInterface
     {
-        $filter = $args['filter'];
-        $value = $args['value'];
+        //$filter = $args['filter'];
+        //$value = $args['value'];
+        
+        $params = explode('/', $request->getAttribute('params'));
+        $filters = [];
+        $values  = [];
         
         try {
+            $this->getFilters($params, $filters, $values);
+            
+            foreach($filters as $filter) {
             FundingSource::validateColumn($filter, $this->logger, $this->cache,
                 $this->db);
-            $records = FundingSource::where($filter, 'like', '%' . $value . '%')->limit(
-                200)->get();
+            }
+            $records = FundingSource::whereRaw(
+                    'LOWER(`' . $filters[0] . '`) like ?', 
+                    ['%' . strtolower($values[0]) . '%']);
+            for($i = 1; $i < count($filters); $i++) {
+                $records = $records->whereRaw(
+                    'LOWER(`' . $filters[$i] . '`) like ?', 
+                    ['%' . strtolower($values[$i]) . '%']);
+            }
+            $records = $records->limit(200)->get();
+            
             $this->logger->debug("FundingSource filter query: ",
                 $this->db::getQueryLog());
             if ($records->isEmpty()) {
