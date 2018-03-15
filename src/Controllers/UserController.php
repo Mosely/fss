42e4f4becaaba99715f4ab370f7ff05f1b630f26
mlhomfeld <mlhomfeld@yahoo.com>
2018-03-15 15:24:48 -0500

    Updated read/readAllWithFilter

--- src/Controllers/UserController.php
+++ src/Controllers/UserController.php
@@ -92,9 +92,9 @@ class UserController extends AbstractController
         ResponseInterface $response, array $args): ResponseInterface
     {
         $id = $args['id'];
-        $args['filter'] = "id";
-        $args['value'] = $id;
-        
+        $params = ['id', $id];
+        $request = $request->withAttribute('params', 
+            implode('/', $params));
         $this->logger->debug("Reading user with id of $id");
         
         // $user = User::findOrFail($id);
@@ -171,11 +171,19 @@ class UserController extends AbstractController
     public function readAllWithFilter(ServerRequestInterface $request,
         ResponseInterface $response, array $args): ResponseInterface
     {
-        $filter = $args['filter'];
-        $value = $args['value'];
+        //$filter = $args['filter'];
+        //$value = $args['value'];
+        
+        $params = explode('/', $request->getAttribute('params'));
+        $filters = [];
+        $values  = [];
         
         try {
+            $this->getFilters($params, $filters, $values);
+            
+            foreach($filters as $filter) {
             User::validateColumn($filter, $this->logger, $this->cache, $this->db);
+            }
             // $records = User::where($filter, 'like', '%' . $value . '%')->get();
             $records = User::with(
                 [
@@ -186,9 +194,16 @@ class UserController extends AbstractController
                         // and gender tables) you will need to handle the
                         // deeper relationships as done here.
                     }
-                ])->where($filter, 'like', '%' . $value . '%')
-                ->limit(200)
-                ->get();
+                ])->whereRaw(
+                    'LOWER(`' . $filters[0] . '`) like ?', 
+                    ['%' . strtolower($values[0]) . '%']);
+            for($i = 1; $i < count($filters); $i++) {
+                $records = $records->whereRaw(
+                    'LOWER(`' . $filters[$i] . '`) like ?', 
+                    ['%' . strtolower($values[$i]) . '%']);
+            }
+            $records = $records->limit(200)->get();
+            
             $this->logger->debug("Users with filter query: ",
                 $this->db::getQueryLog());
             if ($records->isEmpty()) {
