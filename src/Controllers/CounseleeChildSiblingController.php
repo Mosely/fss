18f57866f6729621ba23303f75aef181ec721ba1
mlhomfeld <mlhomfeld@yahoo.com>
2018-03-15 14:17:10 -0500

    Updated read/readAllWithFilter

--- src/Controllers/CounseleeChildSiblingController.php
+++ src/Controllers/CounseleeChildSiblingController.php
@@ -92,9 +92,9 @@ class CounseleeChildSiblingController extends AbstractController
         ResponseInterface $response, array $args): ResponseInterface
     {
         $id = $args['id'];
-        $args['filter'] = "id";
-        $args['value'] = $id;
-        
+        $params = ['id', $id];
+        $request = $request->withAttribute('params', 
+            implode('/', $params));
         $this->logger->debug("Reading CounseleeChildSibling with id of $id");
         
         return $this->readAllWithFilter($request, $response, $args);
@@ -165,19 +165,34 @@ class CounseleeChildSiblingController extends AbstractController
     public function readAllWithFilter(ServerRequestInterface $request,
         ResponseInterface $response, array $args): ResponseInterface
     {
-        $filter = $args['filter'];
-        $value = $args['value'];
+        //$filter = $args['filter'];
+        //$value = $args['value'];
+        
+        $params = explode('/', $request->getAttribute('params'));
+        $filters = [];
+        $values  = [];
         
         try {
+            $this->getFilters($params, $filters, $values);
+            
+            foreach($filters as $filter) {
             CounseleeChildSibling::validateColumn($filter, $this->logger,
                 $this->cache, $this->db);
+            }
             $records = CounseleeChildSibling::with(
                 [
                     'CounseleeChild',
                     'Gender'
-                ])->where($filter, 'like', '%' . $value . '%')
-                ->limit(200)
-                ->get();
+                ])->whereRaw(
+                    'LOWER(`' . $filters[0] . '`) like ?', 
+                    ['%' . strtolower($values[0]) . '%']);
+            for($i = 1; $i < count($filters); $i++) {
+                $records = $records->whereRaw(
+                    'LOWER(`' . $filters[$i] . '`) like ?', 
+                    ['%' . strtolower($values[$i]) . '%']);
+            }
+            $records = $records->limit(200)->get();
+            
             $this->logger->debug("CounseleeChildSibling filter query: ",
                 $this->db::getQueryLog());
             if ($records->isEmpty()) {
