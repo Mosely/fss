47345f3929d693072ef8f17481d656262658fad3
djhiggs <djhiggs1@gmail.com>
2018-07-02 20:23:42 -0500

    Forgot to Refactor One Controller

--- src/Controllers/FundingSourceController.php
+++ src/Controllers/FundingSourceController.php
@@ -1,17 +1,10 @@
 <?php
 namespace FSS\Controllers;
 
-use FSS\Models\FundingSource;
-use FSS\Schemas\FundingSourceSchema;
 use FSS\Utilities\Cache;
 use Illuminate\Database\Capsule\Manager;
 use League\OAuth2\Server\AuthorizationServer;
 use Monolog\Logger;
-use Neomerx\JsonApi\Encoder\Encoder;
-use Neomerx\JsonApi\Encoder\EncoderOptions;
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\ServerRequestInterface;
-use Exception;
 
 /**
  * The controller for funding_source-related actions.
@@ -80,20 +73,7 @@ class FundingSourceController extends AbstractController
      *      )
      *      )
      */
-    public function read(ServerRequestInterface $request,
-        ResponseInterface $response, array $args): ResponseInterface
-    {
-        $id = $args['id'];
-        $params = [
-            'id',
-            $id
-        ];
-        $request = $request->withAttribute('params', implode('/', $params));
-        // $this->logger->info("Reading FundingSource with id of $id");
-        $this->logger->debug("Reading FundingSource with id of $id");
-        
-        return $this->readAllWithFilter($request, $response, $args);
-    }
+ 
 
     /**
      *
@@ -107,23 +87,7 @@ class FundingSourceController extends AbstractController
      *      )
      *      )
      */
-    public function readAll(ServerRequestInterface $request,
-        ResponseInterface $response, array $args): ResponseInterface
-    {
-        $records = FundingSource::limit(200)->get();
-        $this->logger->debug("All FundingSource query: ",
-            $this->db::getQueryLog());
-        // $records = Funding_source::all();
-            $encoder = Encoder::instance([
-                FundingSource::class => FundingSourceSchema::class,
-            ], new EncoderOptions(JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT,
-                $request->getUri()->getScheme() . '://' .
-                $request->getUri()->getHost()));
-            return $response->withJson(
-                json_decode(
-                    $encoder->encodeData($records)));
-
-    }
+  
 
     /**
      *
@@ -154,66 +118,7 @@ class FundingSourceController extends AbstractController
      *      )
      *      )
      */
-    public function readAllWithFilter(ServerRequestInterface $request,
-        ResponseInterface $response, array $args): ResponseInterface
-    {
-        // $filter = $args['filter'];
-        // $value = $args['value'];
-        $params = explode('/', $request->getAttribute('params'));
-        $filters = [];
-        $values = [];
-        
-        try {
-            $this->getFilters($params, $filters, $values);
-            
-            foreach ($filters as $filter) {
-                FundingSource::validateColumn($filter, $this->logger,
-                    $this->cache, $this->db);
-            }
-            $records = FundingSource::whereRaw(
-                'LOWER(`' . $filters[0] . '`) like ?',
-                [
-                    '%' . strtolower($values[0]) . '%'
-                ]);
-            for ($i = 1; $i < count($filters); $i ++) {
-                $records = $records->whereRaw(
-                    'LOWER(`' . $filters[$i] . '`) like ?',
-                    [
-                        '%' . strtolower($values[$i]) . '%'
-                    ]);
-            }
-            $records = $records->limit(200)->get();
-            
-            $this->logger->debug("FundingSource filter query: ",
-                $this->db::getQueryLog());
-            if ($records->isEmpty()) {
-                return $response->withJson(
-                    [
-                        "success" => true,
-                        "message" => "No FundingSource found",
-                        "data" => $records
-                    ], 404);
-            }
-            $encoder = Encoder::instance([
-                FundingSource::class => FundingSourceSchema::class,
-            ], new EncoderOptions(JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT,
-                $request->getUri()->getScheme() . '://' .
-                $request->getUri()->getHost()));
-            if ($records->count() == 1) {
-                $records = $records->first();
-            }
-            return $response->withJson(
-                json_decode(
-                    $encoder->encodeData($records)));
-
-        } catch (Exception $e) {
-            return $response->withJson(
-                [
-                    "success" => false,
-                    "message" => "Error occured: " . $e->getMessage()
-                ], 400);
-        }
-    }
+ 
 
     /**
      *
@@ -228,39 +133,7 @@ class FundingSourceController extends AbstractController
      *      )
      *      )
      */
-    public function create(ServerRequestInterface $request,
-        ResponseInterface $response, array $args): ResponseInterface
-    {
-        // Make sure the frontend only puts the name attribute
-        // on form elements that actually contain data
-        // for the record.
-        $recordData = $request->getParsedBody();
-        try {
-            foreach ($recordData as $key => $val) {
-                FundingSource::validateColumn($key, $this->logger, $this->cache,
-                    $this->db);
-                $this->logger->debug("POST values: ", [
-                    $key . " => " . $val
-                ]);
-            }
-            $recordData['updated_by'] = $request->getAttribute('oauth_user_id');
-            $recordId = FundingSource::insertGetId($recordData);
-            $this->logger->debug("FundingSource create query: ",
-                $this->db::getQueryLog());
-            return $response->withJson(
-                [
-                    "success" => true,
-                    "message" => "FundingSource $recordId has been created.",
-                    "id" => $recordId
-                ], 200, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
-        } catch (Exception $e) {
-            return $response->withJson(
-                [
-                    "success" => false,
-                    "message" => "Error occured: " . $e->getMessage()
-                ], 400);
-        }
-    }
+ 
 
     /**
      *
@@ -283,38 +156,7 @@ class FundingSourceController extends AbstractController
      *      )
      *      )
      */
-    public function update(ServerRequestInterface $request,
-        ResponseInterface $response, array $args): ResponseInterface
-    {
-        // $id = $args['id'];
-        $recordData = $request->getParsedBody();
-        try {
-            $updateData = [];
-            foreach ($recordData as $key => $val) {
-                FundingSource::validateColumn($key, $this->logger, $this->cache,
-                    $this->db);
-                $updateData = array_merge($updateData,
-                    [
-                        $key => $val
-                    ]);
-            }
-            $updateData['updated_by'] = $request->getAttribute('oauth_user_id');
-            $recordId = FundingSource::update($updateData);
-            $this->logger->debug("FundingSource update query: ",
-                $this->db::getQueryLog());
-            return $response->withJson(
-                [
-                    "success" => true,
-                    "message" => "Updated FundingSource $recordId"
-                ], 200, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
-        } catch (Exception $e) {
-            return $response->withJson(
-                [
-                    "success" => false,
-                    "message" => "Error occured: " . $e->getMessage()
-                ], 400);
-        }
-    }
+ 
 
     /**
      *
@@ -337,26 +179,5 @@ class FundingSourceController extends AbstractController
      *      )
      *      )
      */
-    public function delete(ServerRequestInterface $request,
-        ResponseInterface $response, array $args): ResponseInterface
-    {
-        $id = $args['id'];
-        try {
-            $record = FundingSource::findOrFail($id);
-            $record->delete();
-            $this->logger->debug("FundingSource delete query: ",
-                $this->db::getQueryLog());
-            return $response->withJson(
-                [
-                    "success" => true,
-                    "message" => "Deleted Funding_source $id"
-                ], 200, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
-        } catch (Exception $e) {
-            return $response->withJson(
-                [
-                    "success" => false,
-                    "message" => "FundingSource not found"
-                ], 404);
-        }
-    }
+ 
 }
