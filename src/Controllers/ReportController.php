388a3fc35ac3e28615812e226ad02482e77543e9
Mosely <djhiggs1@gmail.com>
2018-07-29 19:02:36 -0500

    Adjusting Model Return Object for Report Generation

--- src/Controllers/ReportController.php
+++ src/Controllers/ReportController.php
@@ -89,12 +89,12 @@ class ReportController extends AbstractController
         //$reportJson = $this->read($request, $response, $args)->getBody();
         //$report = json_decode($reportJson, false);
         $report = $records;
-        print("<pre>");
-        print_r($report);
-        print("</pre>");
-        return $response;
+        //print("<pre>");
+        //print_r($report);
+        //print("</pre>");
+        //return $response;
         
-        $report = $report['data']['attributes'];
+        //$report = $report['data']['attributes'];
         $columns = $report->report_column;
         $reportName = $report->name;
         $reportType = $report->rtype;
