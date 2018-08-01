48241dc02228512b7d1e859994fabd234cc43031
Mosely <djhiggs1@gmail.com>
2018-08-01 15:43:10 -0500

    For Reports, Overriding Defualt Attributes

--- src/Models/Report.php
+++ src/Models/Report.php
@@ -2,6 +2,7 @@
 namespace FSS\Models;
 
 use Illuminate\Database\Capsule\Manager as DB;
+use Illuminate\Support\Collection as BaseCollection;
 use FSS\Utilities\ReportGenerator;
 
 /**
@@ -31,6 +32,49 @@ class Report extends AbstractModel
         'rtype',
         'updated_by'
     );
+    
+    /**
+     * Cast an attribute to a native PHP type.
+     *
+     * @param  string  $key
+     * @param  mixed  $value
+     * @return mixed
+     */
+    protected function castAttribute($key, $value)
+    {
+        if (! is_null($value)) {
+            return parent::castAttribute($key, $value);
+        }
+        
+        switch ($this->getCastType($key)) {
+            case 'int':
+            case 'integer':
+                return (int) 0;
+            case 'real':
+            case 'float':
+            case 'double':
+                return (float) 0;
+            case 'string':
+                return '';
+            case 'bool':
+            case 'boolean':
+                return false;
+            case 'object':
+            case 'array':
+            case 'json':
+                return [];
+            case 'collection':
+                return new BaseCollection();
+            case 'date':
+                return $this->asDate('0000-00-00');
+            case 'datetime':
+                return $this->asDateTime('0000-00-00');
+            case 'timestamp':
+                return $this->asTimestamp('0000-00-00');
+            default:
+                return $value;
+        }
+    }
 
     public function reportColumn()
     {
