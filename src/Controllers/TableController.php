1cceb4a928a5fb66958e373c976aa35c5b8cc493
Mosely <djhiggs1@gmail.com>
2018-08-18 21:42:12 -0500

    Rearranging Table Object

--- src/Controllers/TableController.php
+++ src/Controllers/TableController.php
@@ -99,10 +99,11 @@ class TableController extends AbstractController implements ControllerInterface
         $tableListing = $this->db::select('SHOW TABLES'); // returns an array of stdObjects
         //$records = $this->db::select('SHOW TABLES'); // returns an array of stdObjects
         $records = [];
-        foreach($tableListing as $table) {
-            $records[] = $table->Tables_in_fss;
-        }
         $theTables = (object)['Tables_in_fss' => $records];
+        foreach($tableListing as $table) {
+            $theTables->Tables_in_fss[] = $table->Tables_in_fss;
+        }
+        
         $theTables = $this->cast($this->modelFullName, $theTables);
         $this->logger->debug("All " . $this->modelName . " query: ",
             $this->db::getQueryLog());
